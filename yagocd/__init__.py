45ac35f2f8fc61bfa81ecf3c39ff74e1398c6194
Grigory Chernyshev <systray@yandex.ru>
2016-04-20 12:57:32 +0200

    Switch to bumpversion.

--- yagocd/__init__.py
+++ yagocd/__init__.py
@@ -29,18 +29,4 @@
 
 from yagocd.client import Client
 
-from pkg_resources import get_distribution, DistributionNotFound
-import os.path
-
-try:
-    _dist = get_distribution('yagocd')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'yagocd')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
+__version__ = '0.1.0'
