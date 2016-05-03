577f416340d5dd91c3baac008594ba710532378e
Grigory Chernyshev <systray@yandex.ru>
2016-05-03 16:10:38 +0200

    Bump version: 0.1.7 → 0.1.8

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ for dirpath, dirname, filenames in os.walk('yagocd'):
 
 setup(
     name='yagocd',
-    version='0.1.7',
+    version='0.1.8',
     description="Yet another Python client for ThoughtWorks GOCD REST API.",
     long_description=readme + '\n\n' + history,
     author="Grigory Chernyshev",
